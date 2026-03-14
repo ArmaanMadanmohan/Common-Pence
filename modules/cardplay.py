c0f25fffb88bd3c402cae420d7bb57ab2f870352
calumvc <calum.cardownie@gmail.com>
2026-03-14 12:56:04 +0000

    stuff

--- modules/cardplay.py
+++ modules/cardplay.py
@@ -6,6 +6,17 @@ class card_type(Enum):
     SELL = 2
     INSTANT = 3
 
+class response_type(Enum):
+    INVEST = 1
+    SELL = 2
+    INSTANT = 3
+    ERROR = 4
+
+class Response:
+    def __init__(self, response_type: response_type, text: str):
+        self.response_type = response_type
+        self.text = text
+
 class Card:
     def __init__(self, id, name, card_type, engine):
         self.id = id
@@ -14,7 +25,6 @@ class Card:
         self.engine = engine
 
     def play_card(self):
-        print("playing")
         if self.card_type == card_type.INVEST:
             self.play_invest()
 
@@ -24,21 +34,27 @@ class Card:
         if self.play_card == card_type.INSTANT:
             self.play_instant()
 
-    def play_invest(self):
+    def play_invest(self) -> Response:
         value = int(input("Select an amount of money to invest\n"))
         if self.id == 0:
             self.engine.invest("stocks", value)
+            return Response(response_type.INVEST, f"Invested {value} in stocks")
 
         elif self.id == 1:
             self.engine.invest("etf", value)
+            return Response(response_type.INVEST, f"Invested {value} in etf")
 
         elif self.id == 2:
             self.engine.invest("bank", value)
+            return Response(response_type.INVEST, f"Invested {value} in bank")
 
         elif self.id == 3:
             self.engine.invest("bond", value)
+            return Response(response_type.INVEST, f"Invested {value} in bond")
 
-    def play_sell(self):
+        return Response(response_type.ERROR, "Failed")
+
+    def play_sell(self) -> Response:
         investment_to_sell = str(input("""
                                        Select an investment to sell:
                                        1. stocks
@@ -54,36 +70,34 @@ class Card:
             case 3: inv = "bank"
             case 4: inv = "bond"
 
-        # if self.id == 4: // sell 15
-        #     engine.sell(15, inv)
-        #
-        # if self.id == 5: // sell 30
-        #     engine.sell(30, inv)
-        #
-        # if self.id == 6: // sell 50
-        #     engine.sell(50, inv)
-        #
-        # if self.id == 7: // sell 100
-        #     engine.sell(100, inv)
+        if self.id == 4:
+            res = engine.sell(15, inv)
+            return Response(response_type.SELL, res)
 
-    def play_instant(self):
+        if self.id == 5:
+            res = engine.sell(30, inv)
+            return Response(response_type.SELL, res)
+
+        if self.id == 6:
+            res = engine.sell(50, inv)
+            return Response(response_type.SELL, res)
+
+        if self.id == 7:
+            res = engine.sell(100, inv)
+            return Response(response_type.SELL, res)
+
+        if self.id == 10:
+            res = engine.sellall()
+            return Response(response_type.SELL, res)
+
+        return Response(response_type.ERROR, "Failed")
+
+    def play_instant(self) -> Response:
         if self.id == 8:
-            pass
+            return Response(response_type.INSTANT, "25")
 
         if self.id == 9: 
-            pass
+            return Response(response_type.INSTANT, "50")
 
-
-cards = [
-        Card(0, "inv_stocks", card_type.INVEST, engine),
-        Card(1, "inv_ETF", card_type.INVEST, engine),
-        Card(2, "inv_bank", card_type.INVEST, engine),
-        Card(3, "inv_bond", card_type.INVEST, engine),
-        Card(4, "sell_15", card_type.SELL, engine),
-        Card(5, "sell_30", card_type.SELL, engine),
-        Card(6, "sell_50", card_type.SELL, engine),
-        Card(7, "sell_100", card_type.SELL, engine),
-        Card(8, "generate_sm", card_type.INSTANT, engine),
-        Card(9, "generate_lg", card_type.INSTANT, engine),
-]
+        return Response(response_type.ERROR, "Failed")
 
