7cd413142e772be3966600e1a35fd31fdf06ce6b
Hassan Mahmoud <hm253@st-andrews.ac.uk>
2026-03-14 12:04:49 +0000

    initial main

---
+++ main.py
@@ -0,0 +1,29 @@
+
+class User(object):
+    def __init__(self):
+        self.round = 0;
+        self.
+
+        
+
+
+round = 0
+stage = 0
+money = 0
+go
+
+# dummy
+def play_card(card_id: int):
+    print("playing card", card_id)
+
+# def play_card(card_id: int, amount: int):
+#     print("playing card", card_id)
+
+if __name__ == "__main__":
+    print("========== WELCOME TO HACKATHON ===========")
+    print("> It is March 14th.")
+    print("> You are BROKE!")
+    print("> Your")
+    while True:
+        play_card(card_id)
+                        
